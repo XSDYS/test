8434b75c6995f8945ed16236c756dd721e6e2f2f
XSDYS <1036720556@qq.com>
2020-05-29 18:51:42 +0800

    first commit

---
+++ src/test/java/com/ftl/itokenconfig/ItokenConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftl.itokenconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItokenConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
